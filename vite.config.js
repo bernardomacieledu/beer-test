acc5608c8c213ae41de18742e9a9b27310fb9e63
scorpion9ine <mauriciomiguel.edu@gmail.com>
2025-12-08 21:51:57 -0300

    16 commit antes dos erros que ocorreram

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,50 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'BrewFest 2024',
-        short_name: 'BrewFest',
-        description: 'Festival de Cervejas Artesanais',
-        theme_color: '#3E2723',
-        background_color: '#FFF8E1',
-        display: 'standalone',
+        name: 'BEERCRAFT 2024',
+        short_name: 'BEERCRAFT',
+        description: 'PROJETO FINAL',
+        theme_color: '#ffffff',
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'png'
+            type: 'image/png',
+            purpose: 'any',
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'png'
-          }
-        ]
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        id: 'com.fake-store.app',
+        orientation: 'any',
+        background_color: '#ffffff',
+        start_url: '.',
+        launch_handler: {
+          client_mode: ['navigate-existing', 'auto'],
+        },
       },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
-    })
-  ]
-})
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
